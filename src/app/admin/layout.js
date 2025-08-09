e77c3d566cc618bc3f8e0b62445d0d231ff2c062
saurav84ya <sauravkumarobra50@gmail.com>
2025-08-09 17:38:03 +0530

    admin panel added

---
+++ src/app/admin/layout.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+export default function Layout({ children }) {
+  return (
+    <div >
+     
+    <div className="max-w-7xl  mx-auto px-3 md:px-7 " >
+          {children}
+     </div>
+
+    </div>
+  );
+}
